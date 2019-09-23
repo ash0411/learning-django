aa9ef6e931dfce78a123ac70586f7934f965bd3e
ash0411 <aashishbora36@gmail.com>
2019-09-23 23:42:55 +0530

    playing with form attributes

--- products/forms.py
+++ products/forms.py
@@ -11,6 +11,16 @@ class ProductForm (forms.ModelForm):
         ]
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+            "placeholder": "Your title",
+            "class" : "new-class-name twoo",
+            "id" : "my-id-for- textarea",
+            "rows" : 20,
+            "columns" : 120
+            }
+        ))
+    price = forms.DecimalField(initial=99)
