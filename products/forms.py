ad97fa92f96b6dff90fadac333e0661d2a5ea1ad
ash0411 <aashishbora36@gmail.com>
2019-09-24 19:23:45 +0530

    forms

--- products/forms.py
+++ products/forms.py
@@ -1,14 +1,45 @@
 from django import forms
 from .models import Product
 
-class ProductForm (forms.ModelForm):
-    class Meta:
-        model =Product
-        fields =[ 
-            'title',
-            'description',
-            'price'
-        ]
+class ProductForm (forms.Form):
+    title = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    email=forms.EmailField()
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+            "placeholder": "Your description", 
+            "class" : "new-class-name twoo",
+            "id" : "my-id-for- textarea",
+            "rows" : 20,
+            "columns" : 120
+            }
+        ))
+    price = forms.DecimalField(initial=99)
+    # class Meta:
+    #     model =Product
+    #     fields =[ 
+    #         'title',
+    #         'description',
+    #         'price'
+    #     ]
+    def clean_title(self,*args,**kwargs):
+        title = self.cleaned_data.get("title")
+        if not "CFE" in title:
+            raise forms.ValidationError("this is not a valid title")
+        if not "news" in title:
+             raise forms.ValidationError("this is not a valid title")
+        return title 
+        
+    def clean_email(self,*args,**kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("this is not a valid email")
+        return email
+             
+
+
+     
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder": "Your title"}))
@@ -16,7 +47,7 @@ class RawProductForm(forms.Form):
         required=False,
         widget=forms.Textarea(
             attrs={
-            "placeholder": "Your title",
+            "placeholder": "Your title", 
             "class" : "new-class-name twoo",
             "id" : "my-id-for- textarea",
             "rows" : 20,
--- products/views.py
+++ products/views.py
@@ -3,29 +3,29 @@ from .forms import ProductForm,RawProductForm
 # Create your views here.
 from .models import Product
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if  request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context={
-        "form": my_form
-    }
-    return render(request,"products/product_create.html",context)
-
 # def product_create_view(request):
-#     form =ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#     form= ProductForm()
+#     my_form = RawProductForm()
+#     if  request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
 #     context={
-#         'form':form
+#         "form": my_form
 #     }
 #     return render(request,"products/product_create.html",context)
+
+def product_create_view(request):
+    form =ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    form= ProductForm()
+    context={
+        'form':form
+    }
+    return render(request,"products/product_create.html",context)
 
 
 def product_detail_view(request):
